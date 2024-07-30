61961d48a5fd8aa130e9efa08de8cde9e0204f76
xjh22222228 <xjh22222228@gmail.com>
2024-07-30 20:53:07 +0800

    1

--- index.js
+++ index.js
@@ -2,11 +2,27 @@
 // See https://github.com/xjh22222228/web-info
 import axios from 'axios';
 
+function getTitle(str) {
+  const regex = /<title>([^<]*)?<\/title>/i;
+  const match = str.match(regex);
+  let title = '';
+  if (match && match[1]) {
+    title = match[1];
+  }
+  return title;
+}
+
 function getIconUrl(str, origin, protocol) {
   protocol = protocol.replace(':', '');
-  const regexGlobal = /<link(.|\s)*?href="(.*?)"/gi;
-  const regex = /href="(.*?)"/i;
-  const match = str.match(regexGlobal);
+  const regexGlobal = /<link(.|\s)*?\/?>/gi;
+  const regex = /href="((.|\s)*?)"/i;
+  let match
+  try {
+     match = str.match(regexGlobal);
+  } catch (error) {
+    console.log('getIconUrl', error.message)
+  }
+  
 
   if (Array.isArray(match)) {
     for (const value of match) {
@@ -58,20 +74,10 @@ function getIconUrl(str, origin, protocol) {
   return '';
 }
 
-function getTitle(str) {
-  const regex = /<title>([^<]*)?<\/title>/i;
-  const match = str.match(regex);
-  let title = '';
-  if (match && match[1]) {
-    title = match[1];
-  }
-  return title;
-}
-
 function getDescription(html) {
   let description = '';
   const regexGlobal = /<meta(.|\s)*?\/?>/gi;
-  const regex = /content="(.*?)"/i;
+  const regex = /content="((.|\s)*?)"/i;
   let match
   try {
     match = html.match(regexGlobal);
@@ -116,9 +122,16 @@ async function getWebInfo(url, axiosConf) {
   try {
     const { origin, protocol } = new URL(url);
     const res = await axios.get(origin, {
+      headers:{
+        'Content-Type': 'text/html;charset=utf-8'
+      },
+      responseType: 'arraybuffer',
       ...axiosConf,
     });
-    const html = res.data;
+    const contentType = (res.headers['content-type'] || '').toLowerCase();
+    // 中文编码
+    const gbk = contentType.includes('gbk') || contentType.includes('gb2312')
+    let html = new TextDecoder(gbk ? 'gbk' : 'utf-8').decode(res.data);
     params.iconUrl = getIconUrl(html, origin, protocol).trim();
     params.title = getTitle(html).trim();
     params.description = getDescription(html).trim();
